3a5be649e359461b6946f024067ef91edd82f389
pipelinelife <64777566+ablanksheet@users.noreply.github.com>
2024-05-29 17:40:43 +0900

    Delete lotto-winner-stores-script.js

--- lotto-winner-stores-script.js
+++
@@ -1,6 +0,0 @@
-// 데이터를 fetch하는 함수
-async function fetchData() {
-    const response = await fetch('https://raw.githubusercontent.com/ablanksheet/lottonumber/main/lottowinnerstores.csv');
-    const data = await response.text();
-    return data;
-}
